b8a91109d7f565f1cb685ef4335bce1a67de7909
Mohammad <Amom99100@gmail.com>
2023-08-15 22:18:08 +0330

    ooooooooooo

--- code.js
+++ code.js
@@ -1,4 +1,3 @@
-
 let $ = document
 
 
@@ -52,6 +51,82 @@ let $ = document
 //     })
 
 // }
+
+
+
+// Arrow Function
+
+// function sum (num1, num2) {
+//     let addNumbers = num1 + num2
+//     return addNumbers
+// }
+
+// const sum = (num1, num2) => num1 + num2
+
+// console.log(sum(8,12));
+
+// const pow = (num1, num2) => {
+//     return num1 ** num2
+// }
+// console.log(pow(2,3));
+
+// const pow = (num1, num2) => num1 ** num2
+
+// const logger = val => console.log(val)
+
+// const generateUser = (id, name, age = 18) => ({id: id, name: name, age: age})
+
+// console.log(sum(8, 12)) // 20
+// console.log(pow(3, 2)); // 9
+// logger('Test Text')
+
+// console.log(generateUser(1, 'Ali', 22));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
